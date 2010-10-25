c34d423fe784419ea12909fdc96094ce99cfbacc
Jonathan Chua <jon2512chua@gmail.com>
2010-10-26 01:42:42 +0800

    Completed mysync.c and tested to work.

--- mysync.c
+++ mysync.c
@@ -5,26 +5,96 @@
    Date:		29/10/10
 */
 
+#include <unistd.h>
 #include "source_collection.h"
+
+#define OPTLIST "am:npruv"
+#define DEFAULT_MOD_TIMES 1
 
 int main(int argc, char *argv[])
 {
-	if (argc < 3)
+	OPTIONS options;
+	TOPLEVELS *tls = malloc(BUFSIZ);
+	DIRECTORYINFO *d;
+	int opt;
+	char **paths;
+	int nDirs = 0;
+
+	/*
+	 * Initialise default without options input.
+	 * Done the long way to be more verbose.
+	 */
+	optind = 1;
+	opterr = 0;
+	options.processHiddens = false;
+	options.timeResolution = DEFAULT_MOD_TIMES;
+	options.performSync = true;
+	options.recursive = false;
+	options.print = false;
+	options.updateStatus = true;
+	options.verbose = false;
+	options.programname = argv[0];
+
+	/*
+	 * Processing options.
+	 */
+	while ((opt = getopt(argc, argv, OPTLIST)) != -1)
 	{
-		//TODO change this to fit sample output
-		fprintf(stderr, "Please enter at least 2 files to be synced.\n");
+		switch (opt)
+		{
+			case 'a':
+				options.processHiddens = !(options.processHiddens);
+				break;
+			case 'm':
+				options.timeResolution = atoi(optarg);
+				break;
+			case 'n':
+				options.performSync = !(options.performSync);
+				break;
+			case 'p':
+				options.print = !(options.print);
+				break;
+			case 'r':
+				options.recursive = !(options.recursive);
+				break;
+			case 'u':
+				options.updateStatus = !(options.updateStatus);
+				break;
+			case 'v':
+				options.verbose = !(options.verbose);
+				break;
+			default:
+				argc = -1;
+		}
+	}
+
+	if (argc - optind + 1 < 3)
+	{
+		fprintf(stderr, "Usage: %s [-amnpruv] dir1 dir2 [dirn ... ]\n", options.programname);
 		exit(EXIT_FAILURE);
 	}
 	else
 	{
 		/*
-		 * TODO
-		 * Basically read in 2 top_lvl_dirs and options, then parses them.
-		 * Calls the three main function to do all the work.
-		 * So only thing required here is to prepare the stuff required.
+		 * Processing the paths array for top level directory.
 		 */
-		option_a();
+		paths = &argv[optind];
 
+		/*
+		 * Counting number of directories supplied.
+		 */
+		char **tempPaths = paths;
+		while (*tempPaths != NULL)
+		{
+			nDirs++;
+			tempPaths++;
+		}
+
+		d = processTopLevelDirectories(tls, paths, nDirs, options);
+		
+		TOPLEVELS tmp = *tls;
+		printFileSystem(d, tmp);
+		syncFiles(d, tmp, options);
 		exit(EXIT_SUCCESS);
 	}
 
