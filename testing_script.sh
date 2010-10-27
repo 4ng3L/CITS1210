c02db64585072845f320297b333d3926c9b1b44d
Jonathan Chua <jon2512chua@gmail.com>
2010-10-27 14:54:24 +0800

    Added -p option to cp to preserve modification time. Otherwise there will be some syncing oddities.

--- testing_script.sh
+++ testing_script.sh
@@ -4,10 +4,10 @@ echo Running testing script
 echo Removing old TestDirectory
 rm -r ./TD
 echo Creating new copy of TestDirectory
-cp -r ./TestDirectory ./TD
+cp -rp ./TestDirectory ./TD
 echo Building program
 make clean
 make
 echo Running mysync
-./mysync ./TD/Dir1 ./TD/Dir2
+./mysync -n ./TD/Dir1 ./TD/Dir2
 echo Finished running testing script
